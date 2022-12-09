c69123f4d82baae021fc36b90ca2c37be64faac7
zhangxiaohu <zhangxiaohu@nti56.com>
2022-12-09 10:15:47 +0800

    DI setter注入

--- Spring-Automatically/src/main/java/com/xiaohu/Dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.xiaohu.Dao;
-
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-
-public interface IUserDao {
-    public void print();
-}
--- Spring-Automatically/src/main/java/com/xiaohu/Service/IUserService.java
+++
@@ -1,6 +0,0 @@
-package com.xiaohu.Service;
-
-public interface IUserService {
-    /*打印一句话*/
-    public void print();
-}
