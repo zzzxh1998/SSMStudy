11c1bd164c28515d70bac0d90b322326694a9a31
zhangxiaohu <zhangxiaohu@nti56.com>
2023-01-10 10:58:31 +0800

    Spring_Mybatis模块

---
+++ Spring_Mybatis/Spring_Mybatis_NoAnnotation_1/src/main/java/com/xiaohu/dao/CustomersDaoMapper.java
@@ -0,0 +1,13 @@
+package com.xiaohu.dao;
+
+import com.xiaohu.entity.Customers;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface CustomersDaoMapper {
+    //查询所有
+    public List<Customers> findAll();
+}
