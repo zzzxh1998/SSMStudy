87a93e922115865189c1228981117b4e68da177f
zhangxiaohu <zhangxiaohu@nti56.com>
2023-01-10 15:36:27 +0800

    Spring_Mybatis模块

---
+++ Comments/xiaohu/dao/CustomersDaoInterface.java
@@ -0,0 +1,11 @@
+package com.xiaohu.dao;
+
+import com.xiaohu.entity.Customers;
+
+import java.util.List;
+
+public interface CustomersDaoInterface {
+
+    //查询所有
+    List<Customers> findAll();
+}
